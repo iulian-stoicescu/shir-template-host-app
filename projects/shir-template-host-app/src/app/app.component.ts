62c75658dde60def3e234c479a0453f8f5595319
Iulian_Stoicescu <iulian_stoicescu@dellteam.com>
2023-03-30 17:40:56 +0300

    - Added a dummy `HomeComponent` that will be displayed on the default route (home) of the app

--- projects/shir-template-host-app/src/app/app.component.ts
+++ projects/shir-template-host-app/src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'shir-template-host-app';
-}
+export class AppComponent {}
